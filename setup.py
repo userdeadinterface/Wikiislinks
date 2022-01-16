3ef4194405b6a76e52d4e7d4b087d95d298128e2
userdeadinterface <88093623+userdeadinterface@users.noreply.github.com>
2022-01-16 17:46:37 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from wikicode import WikisGetLink
  
 setup(name='Wikiislinks',
-      version='v1.0.0',
+      version='1.0.0',
       description='Gets link for Icelandic Wikipedia Page from keyword.',
       author='userdeadinterface',
       url='https://github.com/userdeadinterface/Wikiislinks/',

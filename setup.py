ae41e6d5c88cbe1a1e789861a76fb864dac09fd7
userdeadinterface <88093623+userdeadinterface@users.noreply.github.com>
2022-01-16 17:49:20 +0000

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='Wikiislinks',
-      version='1.0.0',
-      description='Gets link for Icelandic Wikipedia Page from keyword.',
-      author='userdeadinterface',
-      url='https://github.com/userdeadinterface/Wikiislinks/',
-      packages=['requests'],
- )
